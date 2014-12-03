31943fdf040674318ed628b4128d46dcca19a05a
Danny Fritz <dannyfritz@gmail.com>
2014-12-03 11:50:31 -0600

    Fix rule tests.

--- test/rule.js
+++ test/rule.js
@@ -1,11 +1,11 @@
 var test = require('tape');
 var cssb = require('css-bouncer');
+var fixtures = require('./fixtures/css');
 
 test('maximum descendants rule tested', function (t) {
 	var linter = cssb();
 	linter.rule('./index.js', {maximum: 3});
-	var result = linter.lint('div .class #id > ul > li.class {}');
-	console.log(result);
+	var result = linter.lint(fixtures.long);
 	t.equal(result.length, 1, 'One error should happen.');
 	t.end();
 });
@@ -13,8 +13,7 @@ test('maximum descendants rule tested', function (t) {
 test('maximum descendants rule no error', function (t) {
 	var linter = cssb();
 	linter.rule('./index.js', {maximum: 3});
-	var result = linter.lint('div .class #id{}');
-	console.log(result);
+	var result = linter.lint(fixtures.short);
 	t.equal(result.length, 0, 'No error should happen.');
 	t.end();
 });
